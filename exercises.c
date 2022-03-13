aa05c2e962eaa208d2294e98cb156f8f926d1c8a
Your Name <you@example.com>
2022-03-13 01:24:56 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -11,7 +11,9 @@ Función que recibe 3 parámetros (a, b y c),
 y en c almacena el valor de la suma de a más b.
 */
 
-void suma(int a, int b, int * c) {
+void suma(int a, int b, int * c){
+   
+}
     
 
 /*
@@ -36,6 +38,7 @@ Utilice la función sumaN.
 */
 
 void sumaNultimos(int a[], int n, int m, int * suma) {
+
 
 }
 
